b2e49a953399b1a11822d8693ea0b980371953ab
ryukstark <manuprosperi04@gmail.com>
2024-11-04 21:44:20 +0000

    Arreglado main.jsx

--- React-Node-Npm/ProyectoBase/react-ManuelProsperi/src/main.jsx
+++ React-Node-Npm/ProyectoBase/react-ManuelProsperi/src/main.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from '../src/App/App'
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
